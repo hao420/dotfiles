36ea96d13727164cc93e6b13903ba5743c7805bf
Hao <1347426876@qq.com>
2026-03-10 20:43:23 +0800

    添加了vim插件

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "Installing dotfiles..."
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/pack/vendor/start ~/.vim/pack/vendor/start
 
 echo "Done."
